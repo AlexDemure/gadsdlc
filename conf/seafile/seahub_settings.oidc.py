62214cd2ad5272a78f441013193e8dc1b2a4893a
alex <alexanderdemure@gmail.com>
2026-05-19 19:46:18 +0500

    nginx

--- conf/seafile/seahub_settings.oidc.py
+++ conf/seafile/seahub_settings.oidc.py
@@ -1,12 +1,19 @@
 import os
 
 
+SERVICE_URL = os.environ["SEAFILE_URL"]
+FILE_SERVER_ROOT = f"{os.environ['SEAFILE_URL']}/seafhttp"
+CSRF_TRUSTED_ORIGINS = [os.environ["SEAFILE_URL"]]
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
 ENABLE_OAUTH = True
 OAUTH_CREATE_UNKNOWN_USER = True
 OAUTH_ACTIVATE_USER_AFTER_CREATION = True
 OAUTH_CLIENT_ID = os.environ["SEAFILE_OIDC_CLIENT_ID"]
 OAUTH_CLIENT_SECRET = os.environ["SEAFILE_OIDC_CLIENT_SECRET"]
 OAUTH_REDIRECT_URL = f"{os.environ['SEAFILE_URL']}/oauth/callback/"
+OAUTH_PROVIDER_DOMAIN = "authentik"
 OAUTH_PROVIDER = "authentik"
 OAUTH_AUTHORIZATION_URL = f"{os.environ['AUTHENTIK_URL']}/application/o/authorize/"
 OAUTH_TOKEN_URL = f"{os.environ['AUTHENTIK_URL']}/application/o/token/"
